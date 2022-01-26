670a46575af6bc04a57086068620837b1b605963
Julkunen Mio <s2000966@edu.bc.fi>
2022-01-26 09:44:10 +0200

    03-task valmis

--- 03-task/src/App.js
+++ 03-task/src/App.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css'
+import Laskuri from './Laskuri'
 
 const App = () => {
-  const [lkm, asetaLkm] = useState(0);
   return (
-    <div>
-      <p>Olet painanut {lkm} kertaa</p>
-      <button onClick={() => asetaLkm(lkm + 1)}>Lisää</button>
-      <button onClick={() => asetaLkm(lkm - 1)}>Vähennä</button>
-      <button onClick={() => asetaLkm(0)}>Nollaa</button>
-    </div>
+    <Laskuri />
   );
 }
 
