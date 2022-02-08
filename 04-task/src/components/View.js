ac22507d1ba33a1ed148e22ccec2c966b4a178d1
Mio Julkunen <mio.julkunen@gmail.com>
2022-02-08 13:58:54 +0200

    04-task muutos?

--- 04-task/src/components/View.js
+++ 04-task/src/components/View.js
@@ -5,10 +5,10 @@ const View = (props) => {
         <section>
             <div className="view-tausta s-teksti view-section">
                 <h2>This is your input:</h2>
-                <p><span className="viewTeksti">First name: </span><span className="viewKentta">{props.firstName}</span></p>
-                <p><span className="viewTeksti">Last name: </span><span className="viewKentta">{props.lastName}</span></p>
-                <p><span className="viewTeksti">Phone: </span><span className="viewKentta">{props.phone}</span></p>
-                <p><span className="viewTeksti">Message: </span><span className="viewKentta">{props.message}</span></p>
+                <p><span className="viewTeksti">First name: </span>{props.firstName}</p>
+                <p><span className="viewTeksti">Last name: </span>{props.lastName}</p>
+                <p><span className="viewTeksti">Phone: </span>{props.phone}</p>
+                <p><span className="viewTeksti">Message: </span>{props.message}</p>
             </div>
         </section>
     )
