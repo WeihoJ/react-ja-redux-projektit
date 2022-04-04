9151c6847422c71d4020e0ce5af1f68c997078c6
Mio Julkunen <mio.julkunen@gmail.com>
2022-04-04 14:28:36 +0300

    mitä vittua?

--- higher-order-functions/src/App.js
+++ higher-order-functions/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  const animals = [
+    { name: 'Fluffykins', species: 'rabbit' },
+    { name: 'Caro', species: 'dog' },
+    { name: 'Hamilton', species: 'dog' },
+    { name: 'Harold', species: 'fish' },
+    { name: 'Ursula', species: 'cat' },
+    { name: 'Jimmy', species: 'fish' }
+  ];
+
+  // console.log(animals)
+
+  // let dogs = []
+  // for (let i = 0; i < animals.length; i++) {
+  //   if (animals[i].species === 'dog') {
+  //     dogs.push(animals[i])
+  //   }
+  // }
+
+  // console.log(dogs)
+
+
+  // const result = animals.filter(animal => {
+  //   return animal.species === 'dog'
+  // });
+
+  // console.log(result)
+
+  const otherAnimals = animals.filter(animal => {
+    return animal.species !== 'dog'
+  });
+
+  console.log(otherAnimals)
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
